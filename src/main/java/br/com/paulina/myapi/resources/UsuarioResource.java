6bfc176e7d8bb1ba83778393b4ba7887ae20ee1d
Paulina Ester Egvarth Rehbein <paulinarehbein@Paulinas-MacBook-Pro.local>
2022-08-29 17:03:57 -0300

    adicionado classe para politica de CORS api totalmente publica

--- src/main/java/br/com/paulina/myapi/resources/UsuarioResource.java
+++ src/main/java/br/com/paulina/myapi/resources/UsuarioResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.paulina.myapi.domain.Usuario;
 import br.com.paulina.myapi.services.UsuarioService;
 
+
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
